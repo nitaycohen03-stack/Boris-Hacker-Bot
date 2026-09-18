dc2e6d79aa9dc5f202d9ae1d80d57cdc297712ae
nitaycohen03-stack <nitaycohen03@gmail.com>
2026-09-18 11:16:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import os
+from flask import Flask
+from threading import Thread
 import discord
 from discord.ext import commands
 
+# Keep-Alive Server
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
+# Discord Bot
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
 @bot.event
 async def on_ready():
-  print(f"Logged in as {bot.user}")
-
+    print(f"Logged in as {bot.user}")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
